5c2c455311a122043b7da9e899b8aae8b8c2c217
adityazzyyzz <165871576+adityazzyyzz@users.noreply.github.com>
2024-04-05 02:10:58 +0000

    updated

---
+++ Arithmetic.py
@@ -0,0 +1,16 @@
+#Problem : to take two integer input from thw user and add both the integer
+
+#Inputs : the inputs are the two numbers entered by the user the input can be float or integer
+
+#output : the output is the sum of two numbers
+
+#constraints : the inputs shoud be in integer format or float
+
+#code :
+a = float(input("enter the first integer:  "))
+b = float(input("enter the second integer:"))
+
+result= a+b
+
+print("the addition of ", a,"and", b "is " , result)
+# validation: the code is testesd with negative, positive integers and floats including zero .
